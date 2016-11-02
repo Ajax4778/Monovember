42b59a7820ea26a01d35496e49ec8bd71c2a00c6
Adithya Manohar <ajax4778@gmail.com>
2016-11-02 13:20:08 -0700

    first Monovember commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,28 @@
+﻿// November 1st, 2016
+// Monovember Day 1
+
+using System;
+
+namespace HelloWorld
+{
+	class HelloWorld
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+
+			string answer;
+			do {
+				Console.WriteLine ("Password?");
+				answer = Console.ReadLine ();
+			} while (!answer.Equals ("password"));
+
+			Console.WriteLine ("----------");
+			Console.WriteLine ("What is your name?");
+			string name = Console.ReadLine ();
+
+			Console.WriteLine ("Hello, {0}!", name);
+
+		}
+	}
+}
