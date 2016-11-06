b387fdfe571919ee65fd851ff39c47fba5c3595c
Adithya Manohar <ajax4778@gmail.com>
2016-11-05 22:50:35 -0700

    set up board marking and winning functions

--- TicTacToe/Board.cs
+++ TicTacToe/Board.cs
@@ -1,4 +1,4 @@
-// Monovember Day 4
+// Monovember Day 4, 5
 
 using System;
 using System.Linq;
@@ -8,6 +8,8 @@ namespace TicTacToe
 	class Board
 	{
 		public string[] grid { get; private set; }
+
+		public string winner { get; set; }
 
 		public Board ()
 		{
@@ -23,9 +25,65 @@ namespace TicTacToe
 			Console.WriteLine ("{0} | {1} | {2}", this.grid [6], this.grid [7], this.grid [8]);
 		}
 
-		public bool isLineWon (string[] line)
+		public void placeMark (int[] pos, string mark)
 		{
+			int markingIdx = this.index (pos);
+			this.grid [markingIdx] = mark;
+		}
+
+		public bool isCellEmpty (int[] pos)
+		{
+			int cellIdx = this.index (pos);
+			return this.grid [cellIdx] == " ";
+		}
+
+		public bool isWinner (string mark)
+		{
+			if (!String.IsNullOrWhiteSpace (this.winner)) {
+				return this.winner == mark;
+			}
+
+			foreach (int i in new int[] { 0, 1, 2}) {
+				string[] row = this.row (i);
+				string[] col = this.column (i);
+				string[] dia = this.diagonal (i);
+
+				if (this.isLineWon (row, mark) || this.isLineWon (col, mark) || this.isLineWon (dia, mark)) {
+					this.winner = mark;
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+
+		public bool isLineWon (string[] line, string mark)
+		{
+			if (line.Length == 0 || line [0] != mark) {
+				return false;
+			}
+
 			return (line [0] != " " && line.Distinct ().ToArray ().Length == 1);
+		}
+
+		public bool isFull ()
+		{
+			foreach (string cell in this.grid) {
+				if (cell == " ") {
+					return false;
+				}
+			}
+
+			return true;
+		}
+
+		public int index (int[] pos)
+		{
+			int i = pos [0];
+			int j = pos [1];
+
+			return (i * 3) + j;
 		}
 
 		public string[] row (int i)
