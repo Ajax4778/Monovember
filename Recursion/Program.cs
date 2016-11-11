195bd86df6d64551a884791cd4eebc3359f73a58
Adithya Manohar <ajax4778@gmail.com>
2016-11-10 23:51:11 -0800

    write recursive fibonacci

---
+++ Recursion/Program.cs
@@ -0,0 +1,33 @@
+﻿// Monovember Day 10
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Recursion
+{
+	class Recursion
+	{
+		public static void Main (string[] args)
+		{
+			fibonacci (11).ForEach (Console.WriteLine);
+		}
+
+		public static List<int> fibonacci (int n)
+		{
+			if (n == 0) {
+				return new List<int> ();
+			} else if (n == 1) {
+				return new List<int> (new int[] { 0 });
+			} else if (n == 2) {
+				return new List<int> (new int[] { 0, 1 });
+			}
+
+			List<int> prev = fibonacci (n - 1);
+			int prevCount = prev.Count;
+			prev.Add (prev [prevCount - 1] + prev [prevCount - 2]);
+
+			return prev;
+		}
+	}
+}
