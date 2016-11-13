eac5dcf58a24d9691f004964ee365b02b0bc0327
Adithya Manohar <ajax4778@gmail.com>
2016-11-12 19:15:59 -0800

    write binary search

--- Recursion/Program.cs
+++ Recursion/Program.cs
@@ -11,6 +11,10 @@ namespace Recursion
 		public static void Main (string[] args)
 		{
 			fibonacci (11).ForEach (Console.WriteLine);
+
+			List<int> list = new List<int> (new int[] { 0, 1, 2, 4, 6, 7, 9 });
+			Console.WriteLine (binarySearch (list, 7)); // => 5
+			Console.WriteLine (binarySearch (list, 8)); // => -1
 		}
 
 		public static List<int> fibonacci (int n)
@@ -28,6 +32,29 @@ namespace Recursion
 			prev.Add (prev [prevCount - 1] + prev [prevCount - 2]);
 
 			return prev;
+		}
+
+		public static int  binarySearch (List<int> list, int target)
+		{
+			int count = list.Count;
+			if (count == 0) {
+				return -1;
+			} else if (count == 1) {
+				return (list [0] == target ? 0 : -1);
+			}
+
+			int mid = count / 2;
+			List<int> left = list.GetRange (0, mid);
+			List<int> right = list.GetRange (mid, count - mid);
+
+			if (list [mid] == target) {
+				return mid;
+			} else if (target < list [mid]) {
+				return binarySearch (left, target);
+			} else {
+				int rightSearch = binarySearch (right, target);
+				return (rightSearch == -1 ? -1 : mid + rightSearch);
+			}
 		}
 	}
 }
